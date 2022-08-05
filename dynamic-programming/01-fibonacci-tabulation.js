5c2da469ab734727ee6cf09a1ddffb5583a7692c
GioF71 <giovanni.fulco@gmail.com>
2022-08-05 17:36:17 +0200

    reviewed js versions

--- dynamic-programming/01-fibonacci-tabulation.js
+++ dynamic-programming/01-fibonacci-tabulation.js
@@ -7,7 +7,7 @@ const fib_alvin = (n) => {
         table[i + 1] += table[i]
         table[i + 2] += table[i]
     }
-    return [table, table.length, table[n]]
+    return [/*table, */table.length, table[n]]
 }
 
 // this version does not create empty trailing elements on the table
@@ -18,7 +18,7 @@ const fib = (n) => {
     for (let i = 2; i <= n; i++) {
         table[i] = table[i - 2] + table[i - 1]
     }
-    return [table, table.length, table[n]]
+    return [/*table, */table.length, table[n]]
 }
 
 console.log(fib_alvin(6))  //8
@@ -27,6 +27,6 @@ console.log(fib_alvin(6))  //8
 //console.log(fib_alvin(50)) //12586269025
 
 console.log(fib(6))  //8
-//console.log(fib(7))  //13
-//console.log(fib(8))  //21
-//console.log(fib(50)) //12586269025
+console.log(fib(7))  //13
+console.log(fib(8))  //21
+console.log(fib(50)) //12586269025
--- dynamic-programming/04-how-sum-tabulation.js
+++ dynamic-programming/04-how-sum-tabulation.js
@@ -41,7 +41,7 @@ console.log(howSum_alvin(7, [2,3]))  // 3,2,2
 console.log(howSum(7, [2,3]))        // 3,2,2
 
 
-// console.log(howSum(7, [5,3,4,7]))    // 4,3
-// console.log(howSum(7, [2,4]))        // Null
-// console.log(howSum(8, [2,3,5]))      // 2,2,2,2
-// console.log(howSum(300, [7,14]))     // Null
+console.log(howSum(7, [5,3,4,7]))    // 4,3
+console.log(howSum(7, [2,4]))        // Null
+console.log(howSum(8, [2,3,5]))      // 2,2,2,2
+console.log(howSum(300, [7,14]))     // Null
