ade57c6590c33754db5f9e47f5b9952c6de35ba3
GioF71 <giovanni.fulco@gmail.com>
2022-08-06 01:05:55 +0200

    all-construct tabulation

---
+++ dynamic-programming/js/08-all-construct-tabulation.js
@@ -0,0 +1,25 @@
+const allConstruct = (target, wordBank) => {
+    const table = Array(target.length + 1)
+        .fill()
+        .map(() => [])
+    table[0] = [[]]
+
+    for (let i = 0; i <= target.length; ++i) {
+        for (let word of wordBank) {
+            if (target.slice(i, i + word.length) === word) {
+                const newCombinations = table[i].map(subArray => [...subArray, word])
+                table[i + word.length].push(...newCombinations)
+            }
+        }
+    }
+    return table[target.length]
+}
+
+console.log(allConstruct("purple", ["purp", "p", "ur", "le", "purpl"]))
+console.log(allConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd", "ef", "c"]))
+    // ab, cd, ef
+    // ab, c, def
+    // abc, def
+    // abcd, ef
+console.log(allConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])) // []
+console.log(allConstruct("aaaaaaaaaaz", ["a", "aa", "aaa", "aaaa", "aaaaa"])) // []
---
+++ dynamic-programming/python/08-all-construct-tabulation.py
@@ -0,0 +1,38 @@
+### Tabulation
+
+def allConstruct(target, wordBank):
+    #print(len(target))
+    table = [None] * (len(target) + 1)
+    #print(len(table))W
+    #for i in range(0, len(target) + 1):
+    #    table[i] = [[]]
+    table[0] = [[]]
+    for i in range(1, len(target) + 1):
+        table[i] = []
+    for i in range(0, len(target) + 1):
+        for word in wordBank:
+            if target[i : i + len(word)] == word:
+                currentCombinations = table[i + len(word)]
+                newCombinations = []
+                for current in table[i]:
+                    tmp = current.copy()
+                    tmp.append(word)
+                    newCombinations.append(tmp)
+                if table[i + len(word)] is None:
+                    table[i + len(word)] = newCombinations
+                else:
+                    entry = table[i + len(word)]
+                    for c in newCombinations:
+                       entry.append(c) 
+    return table[len(target)]
+
+print(allConstruct("purple", ["purp", "p", "ur", "le", "purpl"]))
+    # purp, le
+    # p, ur, p, le
+print(allConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd", "ef", "c"]))
+    # ab, cd, ef
+    # ab, c, def
+    # abc, def
+    # abcd, ef
+print(allConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])) # []
+print(allConstruct("aaaaaaaaaaaaaaaaaaaaazzzzzz", ["a", "aa", "aaa", "aaaa", "aaaaa"])) # []
