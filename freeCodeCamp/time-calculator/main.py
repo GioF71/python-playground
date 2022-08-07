f1cbc573ae5a5bae26cb1a3a75476649aa8af587
GioF71 <giovanni.fulco@gmail.com>
2022-08-07 22:57:09 +0200

    time-calculator complete

--- freeCodeCamp/time-calculator/main.py
+++ freeCodeCamp/time-calculator/main.py
@@ -3,8 +3,21 @@ from time_calculator import add_time
 from unittest import main
 
 
-print(add_time("11:06 PM", "2:02"))
+#print(add_time("11:06 PM", "2:02"))
+print(add_time("3:30 PM", "2:12")) #"5:42 PM"
+print(add_time("11:55 AM", "3:12")) #"3:07 PM"
+print(add_time("9:15 PM", "5:30")) #"2:45 AM (next day)"
+print(add_time("11:40 AM", "0:25")) #"12:05 PM"
+print(add_time("2:59 AM", "24:00")) #"2:59 AM (next day)"
+print(add_time("11:59 PM", "24:05")) #"12:04 AM (2 days later)"
+print(add_time("8:16 PM", "466:02")) #"6:18 AM (20 days later)"
+print(add_time("5:01 AM", "0:00")) #"5:01 AM"
+print(add_time("3:30 PM", "2:12", "Monday")) #"5:42 PM, Monday"
+print(add_time("2:59 AM", "24:00", "saturDay")) #"2:59 AM, Sunday (next day)"
+print(add_time("11:59 PM", "24:05", "Wednesday")) #"12:04 AM, Friday (2 days later)"
+print(add_time("8:16 PM", "466:02", "tuesday")) #"6:18 AM, Monday (20 days later)"
 
+print(add_time("11:59 PM", "24:05", "Wednesday")) #"12:04 AM, Friday (2 days later)"
 
 # Run unit tests automatically
 main(module='test_module', exit=False)
--- freeCodeCamp/time-calculator/time_calculator.py
+++ freeCodeCamp/time-calculator/time_calculator.py
@@ -1,26 +1,32 @@
 am_pm = ["AM", "PM"]
 _dow = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
 
-def _parse_start(start):
+def _start_to_minutes(start):
+    return __start_tuple_to_minutes(__parse_start_to_tuple(start))
+
+def _duration_to_minutes(duration):
+    return __duration_tuple_to_minutes(__parse_duration_to_tuple(duration))
+
+def __parse_start_to_tuple(start):
     period = start[len(start) - 2:len(start)]
     ndx = start.index(":")
     h = start[0:ndx]
     m = start[ndx + 1:ndx + 3]
     return (h, m, period)
 
-def _start_to_minutes(start_tuple):
+def __start_tuple_to_minutes(start_tuple):
     h = int(start_tuple[0])
     m = int(start_tuple[1])
     period = 1 if start_tuple[2] == "PM" else 0
     return m + ((period * 12) * 60 + h * 60)
 
-def _parse_duration(duration):
+def __parse_duration_to_tuple(duration):
     ndx = duration.index(":")
     h = duration[0:ndx]
     m = duration[ndx + 1:len(duration)]
     return (h, m)
 
-def _duration_to_minutes(duration_tuple):
+def __duration_tuple_to_minutes(duration_tuple):
     h = int(duration_tuple[0])
     m = int(duration_tuple[1])
     return m + (h * 60)
@@ -32,22 +38,39 @@ def _sum_times(start_minutes, duration_minutes):
     d = int((total_min - m - (h * 60)) / (60 * 24))
     return (d, h, m)
 
-def add_time(start, duration):
-    period = start[len(start) - 2:len(start)]
-    ndx = start.index(":")
-    h = start[0:ndx]
-    m = start[ndx + 1:ndx + 3]
+def _get_minutes(sum_in_minutes):
+    return sum_in_minutes % 60
 
-    return ""
+def _get_hours(sum_in_minutes):
+    return (int) (sum_in_minutes / 60) 
 
-print(_parse_start("12:15 AM"))
-print(_start_to_minutes(_parse_start("12:15 AM")))
-print(_parse_duration("24:05"))
-print(_duration_to_minutes(_parse_duration("24:05")))
-print(_sum_times(_start_to_minutes(_parse_start("12:15 AM")), _duration_to_minutes(_parse_duration("24:05"))))
+def _get_day_index(dayOfWeek):
+    return _dow.index(dayOfWeek.lower())
 
-print(_parse_start("6:15 PM"))
-print(_start_to_minutes(_parse_start("6:15 PM")))
-print(_parse_duration("466:02"))
-print(_duration_to_minutes(_parse_duration("466:02")))
-print(_sum_times(_start_to_minutes(_parse_start("6:15 PM")), _duration_to_minutes(_parse_duration("466:02"))))
+def add_time(start, duration, dayOfWeek = None):
+    sum_tuple = _sum_times(_start_to_minutes(start), _duration_to_minutes(duration))
+
+    # decompose 
+    h = sum_tuple[1]
+    period = "PM" if h >= 12 else "AM"
+    hourStr = 12 if h == 0 else str(h) if h <= 12 else str(h - 12)
+    minStr = str(sum_tuple[2])     
+    
+    # base h:mm AM|PM covered
+    result = "{}:{} {}".format(hourStr, minStr.rjust(2, '0'), period)
+
+    # how many days more?
+    day = sum_tuple[0]
+
+    # day of week
+    if dayOfWeek is not None:
+        result_day = _dow[(_get_day_index(dayOfWeek) + day) % len(_dow)]
+        result += ", {}".format(result_day.capitalize())
+
+    # prepare days later
+    days = sum_tuple[0]
+    if (days > 0):
+        day_cnt = " (next day)" if days == 1 else " ({} days later)".format(days)
+        result += day_cnt
+
+    return result
