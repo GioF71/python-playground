af80b6490725b3da461287752a753430cea1e0eb
GioF71 <giovanni.fulco@gmail.com>
2022-08-10 01:16:12 +0200

    cleaning up

--- freeCodeCamp/arithmetic-arranger/arithmetic_arranger.py
+++ freeCodeCamp/arithmetic-arranger/arithmetic_arranger.py
@@ -1,3 +1,13 @@
+class OpDef:
+    def __init__(self, a, b, op):
+        self.a = a
+        self.b = b
+        self.op = op
+
+    def get_a(self): return self.a
+    def get_b(self): return self.b
+    def get_op(self): return self.op
+
 def arithmetic_arranger(functions, show_answer = False):
     num_ops = len(functions) if functions is not None else 0
     if num_ops > 5:
@@ -8,7 +18,7 @@ def arithmetic_arranger(functions, show_answer = False):
     sum_index = 0
     subtract_index = 1
     operators = ["+", "-"]
-    aList = []
+    op_data = []
     bList = []
     opList = []
     wList = []
@@ -34,23 +44,23 @@ def arithmetic_arranger(functions, show_answer = False):
         result = (int(a) + int(b)) if op == 0 else int(a) - int(b)
         resultList.append(result)
         w = max(ndx, len(f) - ndx - 1)
-        aList.append(a)
         bList.append(b)
         opList.append(op)
         wList.append(w)
+        op_data.append(OpDef(a, b, op))
 
     # print lines
     first_line = ""
     second_line = ""
     third_line = ""
     fourth_line = ""
-    for i in range(0, len(aList)):
-        first_line += "{addendum:>{width}}".format(addendum = aList[i], width = wList[i] + 2)
-        second_line += "{op} {addendum:>{width}}".format(op = operators[opList[i]], addendum = bList[i], width = wList[i])
+    for i in range(0, len(op_data)):
+        first_line += "{addendum:>{width}}".format(addendum = op_data[i].get_a(), width = wList[i] + 2)
+        second_line += "{op} {addendum:>{width}}".format(op = operators[op_data[i].get_op()], addendum = op_data[i].get_b(), width = wList[i])
         third_line += "-" * (wList[i] + 2)
         if show_answer:
             fourth_line += "{answ:>{width}}".format(answ = resultList[i], width = wList[i] + 2)
-        if i < (len(aList) - 1):
+        if i < (len(op_data) - 1):
             second_line += op_separator
             first_line += op_separator
             third_line += op_separator
