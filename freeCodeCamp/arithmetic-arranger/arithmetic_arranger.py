74ce8b368ab3e178f78fe17e4679f03e8f9cebaa
GioF71 <giovanni.fulco@gmail.com>
2022-08-11 06:08:47 +0200

    operator as function

--- freeCodeCamp/arithmetic-arranger/arithmetic_arranger.py
+++ freeCodeCamp/arithmetic-arranger/arithmetic_arranger.py
@@ -1,15 +1,40 @@
+class Operator:
+    def __init__(self, symbol, executor):
+        self.symbol = symbol
+        self.executor = executor
+    
+    def get_symbol(self): return self.symbol
+    def get_executor(self): return self.executor
+
+operator_array = [Operator("+", lambda a, b: a + b), Operator("-", lambda a, b: a - b)]
+operator_dict = { i.get_symbol(): i for i in operator_array}
+
 class OpDef:
-    def __init__(self, a, b, op):
+    def __init__(self, a, b, operator, op, width, result):
         self.a = a
         self.b = b
+        self.operator = operator
         self.op = op
+        self.width = width
+        self.result = result
 
     def get_a(self): return self.a
     def get_b(self): return self.b
+    def get_operator(self): return self.operator
     def get_op(self): return self.op
+    def get_width(self): return self.width
+    def get_result(self): return self.result
+
+def contains_operator(function, operators):
+    for operator in operators:
+        ndx = function.find(operator)
+        if ndx != -1:
+            return operator, ndx
+    return None
 
 def arithmetic_arranger(functions, show_answer = False):
-    num_ops = len(functions) if functions is not None else 0
+    functionArray = functions if functions is not None else []
+    num_ops = len(functionArray)
     if num_ops > 5:
         return "Error: Too many problems."
     max_digits = 4
@@ -19,18 +44,25 @@ def arithmetic_arranger(functions, show_answer = False):
     subtract_index = 1
     operators = ["+", "-"]
     op_data = []
-    wList = []
-    resultList = []
-    for current in functions if functions is not None else []:
+    op_operator = None
+    for current in functionArray:
         f = current.replace(" ", "")
+        contains_operator_result = contains_operator(f, operators)
+        if contains_operator_result == None:
+            return "Error: Operator must be '+' or '-'."    
+        op_operator = contains_operator_result[0]
+        ndx = contains_operator_result[1]
+
         op = None
         ndx = f.find("+")
         if ndx != -1:
             op = sum_index
+            op_operator = "+"
         else:
             ndx = f.find("-")
             if (ndx != -1):
                 op = subtract_index
+                op_operator = "-"
             else:
                 return "Error: Operator must be '+' or '-'."
         if (ndx) > max_digits or (len(f) - ndx - 1) > max_digits:
@@ -39,30 +71,31 @@ def arithmetic_arranger(functions, show_answer = False):
         b = f[ndx + 1:]
         if not (a.isnumeric() and b.isnumeric()):
             return "Error: Numbers must only contain digits."
-        result = (int(a) + int(b)) if op == 0 else int(a) - int(b)
-        resultList.append(result)
-        w = max(ndx, len(f) - ndx - 1)
-        wList.append(w)
-        op_data.append(OpDef(a, b, op))
+        #result = (int(a) + int(b)) if op == 0 else int(a) - int(b)
+        result = operator_dict[op_operator].get_executor()(int(a), int(b))
+        width = max(ndx, len(f) - ndx - 1)
+        op_data.append(OpDef(a = a, b = b, operator = op_operator, op = op, width = width, result = result))
 
-    # print lines
-    first_line = ""
-    second_line = ""
-    third_line = ""
-    fourth_line = ""
-    for i in range(0, len(op_data)):
-        first_line += "{addendum:>{width}}".format(addendum = op_data[i].get_a(), width = wList[i] + 2)
-        second_line += "{op} {addendum:>{width}}".format(op = operators[op_data[i].get_op()], addendum = op_data[i].get_b(), width = wList[i])
-        third_line += "-" * (wList[i] + 2)
-        if show_answer:
-            fourth_line += "{answ:>{width}}".format(answ = resultList[i], width = wList[i] + 2)
-        if i < (len(op_data) - 1):
-            second_line += op_separator
-            first_line += op_separator
-            third_line += op_separator
+    # return output lines
+    resultStr = None
+    if (num_ops > 0):
+        first_line = ""
+        second_line = ""
+        third_line = ""
+        fourth_line = "" if show_answer else None
+        for i in range(0, len(op_data)):
+            first_line += "{addendum:>{width}}".format(addendum = op_data[i].get_a(), width = op_data[i].get_width() + 2)
+            second_line += "{op} {addendum:>{width}}".format(op = operators[op_data[i].get_op()], addendum = op_data[i].get_b(), width = op_data[i].get_width())
+            third_line += "-" * (op_data[i].get_width() + 2)
             if show_answer:
-                fourth_line += op_separator
-    resultStr = first_line + "\n" + second_line + "\n" + third_line
-    if show_answer == True:
-        resultStr += "\n" + fourth_line
+                fourth_line += "{answ:>{width}}".format(answ = op_data[i].get_result(), width = op_data[i].get_width() + 2)
+            if i < (len(op_data) - 1):
+                second_line += op_separator
+                first_line += op_separator
+                third_line += op_separator
+                if show_answer:
+                    fourth_line += op_separator
+        resultStr = first_line + "\n" + second_line + "\n" + third_line
+        if show_answer == True:
+            resultStr += "\n" + fourth_line
     return resultStr
--- freeCodeCamp/arithmetic-arranger/main.py
+++ freeCodeCamp/arithmetic-arranger/main.py
@@ -2,7 +2,19 @@
 from pytest import main
 
 from arithmetic_arranger import arithmetic_arranger
+from arithmetic_arranger import Operator
+from arithmetic_arranger import contains_operator
 
+print(contains_operator("32+698", ["+", "-"]))
+print(contains_operator("3230-698", ["+", "-"]))
+print(contains_operator("3230*698", ["+", "-"]))
+
+
+operator_array = [Operator("+", lambda a, b: a + b), Operator("-", lambda a, b: a - b)]
+operator_dict = { i.get_symbol(): i for i in operator_array}
+
+#print(operator_array[0].get_executor()(3, 5))
+print(operator_dict["+"].get_executor()(3, 5))
 
 print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
 print(arithmetic_arranger(['32 - 698', '1 - 3801', '45 + 43', '123 + 49', '988 + 40'], True))
